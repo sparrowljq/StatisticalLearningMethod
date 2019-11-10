e24fa9f84d82c5a04a8d2d29f82dd22b21a5a5ef
LIUJQ <459049715@qq.com>
2019-11-10 21:15:25 +0800

    对偶感知机的代码提交

--- DualPerceptron.py
+++ DualPerceptron.py
@@ -1,3 +1,4 @@
+# 对偶感知机的代码
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,19 +16,42 @@ for i in range(len(p_x)):
     for j in range(len(p_x)):
         # dot函数表示矩阵的内积
         Gram[i][j] = np.dot(p_x[i], p_x[j])
-print(Gram)
+
 
 # 初始化α1,α2,α3的值
 afa = np.array([0, 0, 0])
+# 初始化法平面截距
+b = 0
 # 初始化学习率η的值
 delta = 1
-# 超平面截距b
-for i in range(100):
+
+
+while 1:
     # 用来表示所有的点是否被正分类
-    choice = -1
-    for j in  len(p_x):
-        for k in range(100):
-            if y[j]== np.sign(afa):
+    choice = 1
+    for i in range(len(p_x)):
+        if y[i] == np.sign(np.dot(afa * y, Gram[i]) + b):
+            continue
+        while 1:
+            # 只要进入该循环表示有点未正确分类
+            choice = -1
+            if y[i] == np.sign(np.dot(afa*y, Gram[i])+b):
+                break
+            else:
+                afa[i] = afa[i]+delta
+                b = b+delta*y[i]
+    if choice == 1:
+        break
+print(afa)
 
+w = np.dot(afa*y, p_x)
+print(b)
 
+line_x = [0, 10]
+line_y = [0, 0]
+for i in range(len(line_x)):
+    line_y[i] = (-w[0]*line_x[i]-b)/w[1]
+
+plt.plot(line_x, line_y)
+plt.savefig("a.png")
 plt.show()
